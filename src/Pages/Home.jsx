0a95930a185bf04450f6e1ef72b15c7d428bb479
Arnov2837 <Shohanurroshid2@gmail.com>
2026-07-30 08:21:11 +0300

    finis book

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Hero from './Hero';
+import AllBooks from '../Component/AllBooks/AllBooks';
 
 const Home = () => {
   return (
     <div>
       
       <Hero></Hero>
+      <AllBooks></AllBooks>
     </div>
   );
 };
